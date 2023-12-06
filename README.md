33a32617e7246bd6ccfdbd522dbffba54fa8437b
Lucas Schroeder <106642825+lucaoinovation@users.noreply.github.com>
2023-12-05 22:05:59 -0300

    projeto completo - inicial

---
+++ README.md
@@ -0,0 +1,2 @@
+# Projeto-Integrador-II
+contém a documentação e demais informações do projeto Integrador II - Engenharia de Software
---
+++ src/app/app.module.ts
@@ -0,0 +1,83 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { Camera } from '@ionic-native/camera';
+
+import { AngularFireModule, FirebaseAppConfig } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+import { AuthService } from './../providers/auth.service';
+import { CapitalizePipe } from './../pipes/capitalize.pipe';
+import { ChatPage } from './../pages/chat/chat';
+import { ChatService } from './../providers/chat.service';
+import { CustomLoggedHeaderComponent } from './../components/custom-logged-header/custom-logged-header.component';
+import { MessageBoxComponent } from './../components/message-box/message-box.component';
+import { MessageService } from './../providers/message.service';
+import { HomePage } from '../pages/home/home';
+import { MyApp } from './app.component';
+import { ProgressBarComponent } from './../components/progress-bar/progress-bar.component';
+import { SigninPage } from './../pages/signin/signin';
+import { SignupPage } from './../pages/signup/signup';
+import { UserInfoComponent } from './../components/user-info/user-info.component';
+import { UserMenuComponent } from './../components/user-menu/user-menu.component';
+import { UserProfilePage } from './../pages/user-profile/user-profile';
+import { UserService } from './../providers/user.service';
+
+const firebaseAppConfig: FirebaseAppConfig = {
+  apiKey: "-",
+  authDomain: "-",
+  databaseURL: "-",
+  projectId: "-",
+  storageBucket: "-",
+  messagingSenderId: "-"
+};
+
+@NgModule({
+  declarations: [
+    CapitalizePipe,
+    ChatPage,
+    CustomLoggedHeaderComponent,
+    HomePage,
+    MessageBoxComponent,
+    MyApp,
+    ProgressBarComponent,
+    SigninPage,
+    SignupPage,
+    UserInfoComponent,
+    UserMenuComponent,
+    UserProfilePage
+  ],
+  imports: [
+    AngularFireModule.initializeApp(firebaseAppConfig),
+    AngularFireAuthModule,
+    AngularFireDatabaseModule,
+    BrowserModule,
+    HttpModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    ChatPage,
+    HomePage,
+    MyApp,
+    SigninPage,
+    SignupPage,
+    UserProfilePage
+  ],
+  providers: [
+    AuthService,
+    ChatService,
+    MessageService,
+    StatusBar,
+    SplashScreen,
+    Camera,
+    UserService,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
